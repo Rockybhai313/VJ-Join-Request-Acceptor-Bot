62d84f6f71da84472cf299557f742465a22787ae
Rockybhai313 <142562669+Rockybhai313@users.noreply.github.com>
2024-12-19 17:39:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
-ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "vjjoinrequetbot")
+API_ID = int(environ.get("API_ID", "21486605"))
+API_HASH = environ.get("API_HASH", "14875c3c88842422a773820bcd6eead2")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6800009762:AAGHgXTD-ckgF0PJF3TwM1S20A_bIFyanbU")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002376177429"))
+ADMINS = int(environ.get("ADMINS", "1555570406"))
+DB_URI = environ.get("DB_URI", "mongodb+srv://star07143star: lSnY4qhTzVPu3Zbq                                                                                                                          @cluster0.5qn5m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", " star07143star")
